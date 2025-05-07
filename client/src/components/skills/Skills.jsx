0311844e095cef2dad819cad62451e6c3009d2cd
alrifatsabbir <alrifatsabbir@gmail.com>
2025-05-08 01:50:16 +0600

    skills-update

--- client/src/components/skills/Skills.jsx
+++ client/src/components/skills/Skills.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { getSkills } from '../../utils/api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAws, faCpanel, faCss3, faGitAlt, faGithub, faJs, faNodeJs, faPython, faReact, faVuejs } from '@fortawesome/free-brands-svg-icons';
+import { faAws, faCpanel, faCss3, faGitAlt, faGithub, faBootstrap,faJs, faNodeJs, faPython, faReact, faVuejs } from '@fortawesome/free-brands-svg-icons';
+import { faLeaf} from '@fortawesome/free-solid-svg-icons'
 
 function Skills() {
   const [skills, setSkills] = useState([]);
@@ -12,22 +13,22 @@ function Skills() {
       .catch((error) => console.error(error));
   }, []);
 
-  const icons = [faCss3, faJs, faGitAlt, faGithub, faNodeJs, faReact, faCpanel, faAws, faPython, faVuejs];
+  const icons = [faReact, faNodeJs, faLeaf, faAws, faGithub, faBootstrap, faJs, faCss3,  faGitAlt, faCpanel,  faPython, faVuejs];
 
   return (
-    <section className="py-16">
+    <section className="py-16 skill-container">
       <div className="container mt-12 mx-auto px-4">
         <h2 className="text-3xl font-bold mb-8 text-center">My Skills</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 skill-container-div">
           {Array.isArray(skills) && skills.map((skill, index) => (
-            <div key={index} className="border rounded-lg p-4">
-              <div className="card bg-base-100 w-46 m-4 sm:w-1/6 pb-5 card-icons">
-                <figure className="px-5 pt-5">
-                  <FontAwesomeIcon icon={icons[index % icons.length]} className="rounded-2xl" size='2x' />
+            <div key={index} className="border-color-skill rounded-lg p-2">
+              <div className="card w-46 m-4 sm:w-5/6 icon-color justify-self-center">
+                <figure className="px-2 pt-2 ">
+                  <FontAwesomeIcon icon={icons[index % icons.length]} className="" size='4x' />
                 </figure>
               </div>
-              <h3 className="text-xl font-semibold mb-2">{skill.name}</h3>
-              <p className="text-gray-600">{skill.description}</p>
+              <h3 className="text-xl text-center font-semibold mb-2">{skill.name}</h3>
+              <p className="text-gray-600 text-center">{skill.description}</p>
             </div>
           ))}
         </div>
--- controllers/skillsController.js
+++ controllers/skillsController.js
@@ -4,10 +4,10 @@ const getSkills = async (_, res, next) => {
         { name: 'React', level: 'Expert', description: 'Building user interfaces with React' },
         { name: 'Node.js', level: 'Expert' },
         { name: 'MongoDB', level: 'Expert' },
-        { name: 'Express.js', level: 'Expert' },
+        { name: 'aws', level: 'Expert' },
         { name: 'GitHub', level: 'Expert'},
         { name: 'Bootstrap', level: 'Expert'},
-        { name: 'Tailwind CSS', level: 'Expert'}
+        { name: 'JavaScript', level: 'Expert'}
       ];
       res.json(skills);
     } catch (error) {
